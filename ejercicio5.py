068c53f14ad1e6f117abce4720002e29d458f3c2
maximo <164915174+Maximopo@users.noreply.github.com>
2024-05-06 16:52:45 -0300

    Create ejercicio5.py

---
+++ ejercicio5.py
@@ -0,0 +1,12 @@
+def ejercicio4():
+    print(n)
+    
+    #Se ingresa 3 números que representan horas, minutos y segundos. Se pide informar el resultado expresado en segundos. Determinar qué tipo de valor es el aconsejable para los datos solicitados
+def multiplicacion():
+    horas = int(input("ingrese las horas:")) 
+    minutos = int(input("ingrese los minutos:"))
+    segundos = int(input("ingrese los segundos:"))
+    resultado = horas *3600 + minutos *60 + segundos
+    print("la cantidad de segundos totales son:", resultado)
+
+multiplicacion()
