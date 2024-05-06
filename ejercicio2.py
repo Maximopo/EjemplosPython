3913fb16f8db53911477c8001400eeb1798efb8d
maximo <164915174+Maximopo@users.noreply.github.com>
2024-05-06 15:22:16 -0300

    Create ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,16 @@
+true = True
+false = False
+valores = [true,5,false,"hola","adios",2]
+
+if len (valores  [3]) < len (valores [4]):
+        print("adios es mayor que hola")
+        print(valores[0])
+    
+else:
+    print("hola es más chico que adios")
+    print[2]
+    print(valores[1]+valores[5])
+    print(valores[1]-valores[5])
+    print(valores[1]*valores[5])
+    print(valores[1]/valores[5])
+ 
