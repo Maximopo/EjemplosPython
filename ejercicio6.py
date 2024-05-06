b7c60f34a505d01bcbbaf7593f0444199303d644
maximo <164915174+Maximopo@users.noreply.github.com>
2024-05-06 16:56:08 -0300

    Update ejercicio6.py

---
+++ ejercicio6.py
@@ -0,0 +1,12 @@
+def ejercicio5():
+    print(n)
+    
+    #Se necesita calcular la superficie de un triángulo, y se dispone solamente de los valores de su base y altura.Definir también que tipo de valor es aconsejable para las variables con la información que se tiene.**No se podrá usar valores fijos en las fórmulas del algoritmo. Solo variables y/o constantes.**
+def multiplicacion():
+    horas = int(input("ingrese las horas:")) 
+    minutos = int(input("ingrese los minutos:"))
+    segundos = int(input("ingrese los segundos:"))
+    resultado = horas *3600 + minutos *360 + segundos
+    print("la cantidad de segundos totales son:", resultado)
+
+multiplicacion()
