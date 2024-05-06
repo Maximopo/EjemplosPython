e76f53d5f1317f30c85fa4e7ec4200e35551d436
maximo <164915174+Maximopo@users.noreply.github.com>
2024-05-06 15:22:11 -0300

    Create ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,12 @@
+#crear una lista de los doce meses del año y que se llame meses
+#Mostrar una pantalla los doce nombres utilizando la función print()
+
+meses = ["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"]
+n = 5
+for mes in range(len (meses)):
+    print(mes)
+
+if n == 5:
+    print(n)
+else:
+    print(n)
