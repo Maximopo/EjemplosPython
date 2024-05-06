d646ea631cd1d8fa637774f55f9d31518e1bca73
maximo <164915174+Maximopo@users.noreply.github.com>
2024-05-06 17:19:18 -0300

    Create ejercicio8.py

---
+++ ejercicio8.py
@@ -0,0 +1,13 @@
+def ejercicio7():
+    print(n)
+    
+    #Dados 2 números enteros, que representan una cantidad parcial y total se pide: Calcular e informar el porcentaje que representa la primera de la segunda. ¿Qué tipo de datos son los recomendados para este algoritmo?
+def promedio():
+    n1 = int(input("ingrese el primer numero:")) 
+    n2 = int(input("ingrese el segundo numero:"))
+    porcentaje = (n2 * n1) / 100
+
+    print("el porcentaje de la primera de la segunda es:", porcentaje)
+    print("el tipo de dato recomendado es el int (el entero)")
+    
+promedio()
