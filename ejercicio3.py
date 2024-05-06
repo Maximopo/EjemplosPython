a3584e7a80df532c72a7a43b308c6438c104279d
maximo <164915174+Maximopo@users.noreply.github.com>
2024-05-06 16:34:33 -0300

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,5 +1,11 @@
-ancho = 5
-largo = 10
-superficie = ancho * largo
-if ancho < largo:
-    print(superficie)
+def ejercicio3():
+    print(n)
+    
+    #Dados 2 números enteros, que representan los lados de un rectángulo se pide informar la superficie del mismo.
+def multiplicacion():
+    base = int(input("ingrese la base del rectangulo")) 
+    altura = int(input("ingrese la altura del rectangulo"))
+    superficie = altura * base
+    print("la superficie del rectangulo es:", superficie)
+    
+multiplicacion()
