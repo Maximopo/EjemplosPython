927a399d6bbdebee760fb5aa4e72c1a22fa9edec
maximo <164915174+Maximopo@users.noreply.github.com>
2024-05-06 17:19:43 -0300

    Update ejercicio9.py

---
+++ ejercicio9.py
@@ -0,0 +1,3 @@
+Dada una fecha que se lee en formato numérico DDMMAAAA (dos números para el día, dos para el mes, cuatro
+para el año), se solicita obtener el día el mes y año por separado en tres variables. (usar descomposición
+matemática)
