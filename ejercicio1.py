c16622c3ef3761e5df2e111dbd681ff308b72911
maximo <164915174+Maximopo@users.noreply.github.com>
2024-05-06 16:23:10 -0300

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,10 +1,13 @@
-
-
+def ejercicio1():
+    print(n)
+    
+    #leer dos numeros enteros, calcular la suma e informar el resultado
+    
 def suma():
-numero1 = int(input("ingrese un número entero")) 
-numero2 = int(input("ingrese otro número entero")) 
-resultado = numero1 + numero2
+    n1 = int(input("ingrese un número entero")) 
+    n2 = int(input("ingrese otro número entero")) 
+    resultado = n1 + n2
 
-print ("el resultado es", resultado)
+    print ("el resultado es: ", resultado)
 
 suma()
