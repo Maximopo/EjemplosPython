dd1995e997c654624fcb03e5546052df7ce38c17
maximo <164915174+Maximopo@users.noreply.github.com>
2024-05-06 17:23:14 -0300

    Update ejercicio9.py

--- ejercicio9.py
+++ ejercicio9.py
@@ -1,3 +1,14 @@
-Dada una fecha que se lee en formato numérico DDMMAAAA (dos números para el día, dos para el mes, cuatro
-para el año), se solicita obtener el día el mes y año por separado en tres variables. (usar descomposición
-matemática)
+def ejercicio9():
+    print(n)
+    
+    #Dada una fecha que se lee en formato numérico DDMMAAAA (dos números para el día, dos para el mes, cuatro para el año), se solicita obtener el día el mes y año por separado en tres variables. (usar descomposición matemática)
+    
+def fecha():
+    dia = int(input("ingrese el dia:")) 
+    mes = int(input("ingrese el mes:"))
+    año = int(input("ingrese el año:"))
+
+    print("el porcentaje de la primera de la segunda es:", porcentaje)
+    print("el tipo de dato recomendado es el int (el entero)")
+    
+fecha()
