88596c8043758dd870b43526cbf91d9d9105b4c3
maximo <164915174+Maximopo@users.noreply.github.com>
2024-05-06 16:38:52 -0300

    Update ejercicio4.py

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,5 +1,11 @@
-anchodecimal = 1.25
-largo = 4
-superficie = anchodecimal * largo
-if anchodecimal < largo:
-    print (superficie)
+def ejercicio4():
+    print(n)
+    
+    #Dado 1 número con decimales, que representa el lado de un cuadrado se pide informar la superficie del mismo.
+def multiplicacion():
+    base = float(input("ingrese la base del rectangulo")) 
+    altura = float(input("ingrese la altura del rectangulo"))
+    superficie = base * altura
+    print("la superficie del rectangulo es:", superficie)
+
+multiplicacion()
