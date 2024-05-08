6b0ee0d82cbca9c1f0dc5bf183c9f802c0e09ef3
Maximopo <164915174+Maximopo@users.noreply.github.com>
2024-05-08 15:39:24 -0300

    Update ejericico11.py

--- ejericico11.py
+++ ejericico11.py
@@ -1,13 +1,3 @@
-def ejercicio10():
+def ejercicio11():
     print(n)
-    #En un curso de ciencias de la computación la calificación final del estudiante se determina a partir delrendimiento en tres aspectos del trabajo. A) Existe una calificación por los exámenes parciales que representa el30% del valor total de la nota final. B) la calificación por los trabajos prácticos corresponde al 20% del valor de lanota final. C) el examen integrador que corresponde al 50% restante. (los valores de las notas pueden ir de 0 a 10)
-def nota():
-    examPar = int (input("Inserte la nota parcial:",)[0:11])
-    tp = int (input("Inserte la nota de los tps:")[0:11])
-    examInt = int (input("Inserte la nota del exámen integral:")[0:11])
-    primeranota = (examPar * 30) / 100
-    segundanota = (tp * 20) / 100
-    terceranota = (examInt * 50) / 100
-    notafinal = primeranota + segundanota + terceranota
-    print ("la nota final del estudiante es:", notafinal)
-nota()
+    #Una concesionaria de autos paga a su personal, un salario de 5500 pesos por mes, mas una comisión del 200pesos por cada auto vendido y un adicional del 5% del valor del auto vendido. Diseñar un algoritmo para calcularel salario total del vendedor. Conociendo solamente el número de autos vendidos y el valor de venta de launidad.
