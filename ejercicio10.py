aa9a432be7dda30a12042d8f6c07f00520a79047
Maximopo <164915174+Maximopo@users.noreply.github.com>
2024-05-08 15:00:59 -0300

    Update ejercicio10.py

--- ejercicio10.py
+++ ejercicio10.py
@@ -1,18 +1,13 @@
-def ejercicio9():
+def ejercicio10():
     print(n)
-    
-    #Dada una fecha que se lee en formato numérico DDMMAAAA (dos números para el día, dos para el mes, cuatro para el año), se solicita obtener el día el mes y año por separado en tres variables. (usar descomposición matemática)
-    
-def fecha():
-    fecha = int(input("ingrese la fecha:" ))
-    año = fecha % 10000
-    fecha = int (fecha / 10000)
-    mes = fecha % 100
-    fecha = int (fecha / 100)
-    dia = fecha 
-    
-    print("el año es:", año)
-    print("el mes es:", mes)
-    print("el dia es:", dia)
-    
-fecha()
+    #En un curso de ciencias de la computación la calificación final del estudiante se determina a partir delrendimiento en tres aspectos del trabajo. A) Existe una calificación por los exámenes parciales que representa el30% del valor total de la nota final. B) la calificación por los trabajos prácticos corresponde al 20% del valor de lanota final. C) el examen integrador que corresponde al 50% restante. (los valores de las notas pueden ir de 0 a 10)
+def nota():
+    examPar = int (input("Inserte la nota parcial:",)[0:11])
+    tp = int (input("Inserte la nota de los tps:")[0:11])
+    examInt = int (input("Inserte la nota del exámen integral:")[0:11])
+    primeranota = (examPar * 30) / 100
+    segundanota = (tp * 20) / 100
+    terceranota = (examInt * 50) / 100
+    notafinal = primeranota + segundanota + terceranota
+    print ("la nota final del estudiante es:", notafinal)
+nota()
