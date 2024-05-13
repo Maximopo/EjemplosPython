8db7b656be44825983a9fc9941bd35b0670c50b1
Maximo <aranguezmaximo@gmail.com>
2024-05-13 15:09:03 -0300

    Update ejercicio6.py

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,7 +1,4 @@
-def ejercicio6():
-    print(n)
-    
-    #Se necesita calcular la superficie de un triángulo, y se dispone solamente de los valores de su base y altura.Definir también que tipo de valor es aconsejable para las variables con la información que se tiene.**No se podrá usar valores fijos en las fórmulas del algoritmo. Solo variables y/o constantes.**
+ #Se necesita calcular la superficie de un triángulo, y se dispone solamente de los valores de su base y altura.Definir también que tipo de valor es aconsejable para las variables con la información que se tiene.**No se podrá usar valores fijos en las fórmulas del algoritmo. Solo variables y/o constantes.**
 def triangulo():
     base = float(input("ingrese la base:")) 
     altura = float(input("ingrese la altura:"))
