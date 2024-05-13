796da069076647829a1be2e5b6ae3af470222d1f
Maximo <aranguezmaximo@gmail.com>
2024-05-13 14:21:52 -0300

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -2,7 +2,7 @@ def ejercicio1():
     print(n)
     
     #leer dos numeros enteros, calcular la suma e informar el resultado
-    
+    #Prueba de Algoritmos: estructura de control (if,else, else if,), repetición (for, while),tipo de datos, declaración de variables, array y listas, diferencias entre lenght, len, back y cons, variables constantes en python, prototipo en js. Todo esto en js y python (a excepción de los especificos)
 def suma():
     n1 = int(input("ingrese un número entero")) 
     n2 = int(input("ingrese otro número entero")) 
