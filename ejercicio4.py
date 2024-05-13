6c53a3b1f9d092ba2d7a5bc287c05b0dbf43c6b9
Maximo <aranguezmaximo@gmail.com>
2024-05-13 15:08:59 -0300

    Update ejercicio4.py

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,7 +1,4 @@
-def ejercicio4():
-    print(n)
-    
-    #Dado 1 número con decimales, que representa el lado de un cuadrado se pide informar la superficie del mismo.
+ #Dado 1 número con decimales, que representa el lado de un cuadrado se pide informar la superficie del mismo.
 def multiplicacion():
     base = float(input("ingrese la base del rectangulo")) 
     altura = float(input("ingrese la altura del rectangulo"))
