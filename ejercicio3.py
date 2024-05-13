7f44b0cbb55b9d58302e583884397036bb3c4830
Maximo <aranguezmaximo@gmail.com>
2024-05-13 15:08:57 -0300

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,7 +1,4 @@
-def ejercicio3():
-    print(n)
-    
-    #Dados 2 números enteros, que representan los lados de un rectángulo se pide informar la superficie del mismo.
+#Dados 2 números enteros, que representan los lados de un rectángulo se pide informar la superficie del mismo.
 def multiplicacion():
     base = int(input("ingrese la base del rectangulo")) 
     altura = int(input("ingrese la altura del rectangulo"))
