5572f27be005f80fd800411fdd26bcaf7ec41c21
Maximo <aranguezmaximo@gmail.com>
2024-05-13 15:09:11 -0300

    Update ejercicio9.py

--- ejercicio9.py
+++ ejercicio9.py
@@ -1,7 +1,4 @@
-def ejercicio9():
-    print(n)
-    
-    #Dada una fecha que se lee en formato numérico DDMMAAAA (dos números para el día, dos para el mes, cuatro para el año), se solicita obtener el día el mes y año por separado en tres variables. (usar descomposición matemática)
+ #Dada una fecha que se lee en formato numérico DDMMAAAA (dos números para el día, dos para el mes, cuatro para el año), se solicita obtener el día el mes y año por separado en tres variables. (usar descomposición matemática)
     
 def fecha():
     fecha = int(input("ingrese la fecha:" ))
