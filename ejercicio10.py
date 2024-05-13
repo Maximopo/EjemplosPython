e37b25c71bea59e37a24ed9bbd4d682cab247be0
Maximo <aranguezmaximo@gmail.com>
2024-05-13 15:08:53 -0300

    Update ejercicio10.py

--- ejercicio10.py
+++ ejercicio10.py
@@ -1,6 +1,4 @@
-def ejercicio10():
-    print(n)
-    #En un curso de ciencias de la computación la calificación final del estudiante se determina a partir delrendimiento en tres aspectos del trabajo. A) Existe una calificación por los exámenes parciales que representa el30% del valor total de la nota final. B) la calificación por los trabajos prácticos corresponde al 20% del valor de lanota final. C) el examen integrador que corresponde al 50% restante. (los valores de las notas pueden ir de 0 a 10)
+#En un curso de ciencias de la computación la calificación final del estudiante se determina a partir delrendimiento en tres aspectos del trabajo. A) Existe una calificación por los exámenes parciales que representa el30% del valor total de la nota final. B) la calificación por los trabajos prácticos corresponde al 20% del valor de lanota final. C) el examen integrador que corresponde al 50% restante. (los valores de las notas pueden ir de 0 a 10)
 def nota():
     examPar = int (input("Inserte la nota parcial:",)[0:11])
     tp = int (input("Inserte la nota de los tps:")[0:11])
