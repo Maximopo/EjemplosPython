1e55b27631f27d4ed6ace4070c4e1df68dd8109f
Maximo <aranguezmaximo@gmail.com>
2024-05-13 15:09:08 -0300

    Update ejercicio8.py

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,11 +1,8 @@
-def ejercicio8():
-    print(n)
-    
-    #Dados 2 números enteros, que representan una cantidad parcial y total se pide: Calcular e informar el porcentaje que representa la primera de la segunda. ¿Qué tipo de datos son los recomendados para este algoritmo?
+#Dados 2 números enteros, que representan una cantidad parcial y total se pide: Calcular e informar el porcentaje que representa la primera de la segunda. ¿Qué tipo de datos son los recomendados para este algoritmo?
 def porcentaje():
     n1 = float(input("ingrese el primer numero:")) 
     n2 = float(input("ingrese el segundo numero:"))
-    porcentaje = (n2 * n1) / 100
+    porcentaje =  n2 / (n1 / 100) 
 
     print("el porcentaje de la primera de la segunda es:%", porcentaje)
     print("el tipo de dato recomendado es el float")
